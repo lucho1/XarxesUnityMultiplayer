89a78792d49ea8be075d2958f4d1b7c4d5d851a5
lucho1 <luchosuaya@hotmail.com>
2021-01-09 01:05:49 +0100

    Online Connectivity Started (see desc.)
    · Made a ConnectionManager to handle connection stuff (server connections, room join & creations...) with lots of callbacks to retrieve errors
    
    · Added support from UI to it, with a list of available created rooms, a button to create a new one, another one to join a random room, an input text for your username...

---
+++ MultiplayerGame/Assets/Networking/ConnectionManager.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class ConnectionManager : MonoBehaviourPunCallbacks
+{
+    public int MaxPlayers = 10;   
+    public string GameVersion = "1.0";
+    public Text Username;
+    
+    [SerializeField]
+    private Text RoomName;
+
+    [SerializeField]
+    private Text ErrorText;
+
+    private Timer m_ErrorTextTime;
+
+    private void Awake()
+    {
+        // This will allow us to use PhotonNetwork.LoadLevel() on the master and all clients in room will sync their level automatically
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+        m_ErrorTextTime = GetComponent<Timer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Connecting to Server");
+
+        PhotonNetwork.NickName = Username.text;
+        PhotonNetwork.GameVersion = GameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    private void Update()
+    {
+        if (ErrorText.IsActive() && m_ErrorTextTime.ReadTime() > 2.0f)
+        {
+            m_ErrorTextTime.Restart();
+            m_ErrorTextTime.Stop();
+
+            ErrorText.text = "Error Text";
+            ErrorText.gameObject.SetActive(false);
+        }
+    }
+
+    // --- Connection Override Callbacks ---
+    public override void OnConnectedToMaster()
+    {
+        print("Connected to MasterServer");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnConnected()
+    {
+        print("Connected to Server");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("Disconnected from Server: " + cause.ToString());
+    }
+
+
+    // ---------- ROOMS ----------
+    public void JoinOrCreateRoom()
+    {
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = (byte)MaxPlayers;
+        PhotonNetwork.JoinOrCreateRoom(RoomName.text, options, TypedLobby.Default);
+    }
+
+    public void JoinRandomRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    // --- Informative Override Callbacks ---
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created Room Successfully", this);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        string error_message = "Error on Room Creation: " + message + " (#" + returnCode + ")";
+
+        ErrorText.text = error_message;
+        ErrorText.gameObject.SetActive(true);
+        m_ErrorTextTime.Start();
+
+        Debug.Log(error_message, this);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined Room Successfully", this);
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        string error_message = "Error on Room Creation: " + message + " (#" + returnCode + ")";
+
+        ErrorText.text = error_message;
+        ErrorText.gameObject.SetActive(true);
+        m_ErrorTextTime.Start();
+
+        Debug.Log(error_message, this);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        string error_message = "Error on Room Creation: " + message + " (#" + returnCode + ")";
+
+        ErrorText.text = error_message;
+        ErrorText.gameObject.SetActive(true);
+        m_ErrorTextTime.Start();
+
+        Debug.Log(error_message, this);
+    }
+}
---
+++ MultiplayerGame/Assets/Networking/RoomListController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomListController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Transform ListContent;
+
+    [SerializeField]
+    private GameObject RoomListElement;
+
+    private List<GameObject> m_ExistingRoomsList = new List<GameObject>();
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach(RoomInfo room in roomList)
+        {
+            if (room.RemovedFromList)
+            {
+                foreach(GameObject obj in m_ExistingRoomsList)
+                {
+                    if(obj.GetComponent<Text>().text == room.Name)
+                    {
+                        m_ExistingRoomsList.Remove(obj);
+                        Destroy(obj);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                GameObject list_element = Instantiate(RoomListElement, ListContent);
+                if (list_element)
+                {
+                    list_element.GetComponentInChildren<Text>().text = room.Name;
+                    m_ExistingRoomsList.Add(list_element);
+                }
+            }
+        }
+    }
+}
