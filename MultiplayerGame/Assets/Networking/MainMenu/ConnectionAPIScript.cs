ae80a07ac7d549ccdc3ad9355fc4002517b2d6b2
lucho1 <luchosuaya@hotmail.com>
2021-01-14 13:25:08 +0100

    Created New Scripts and Deleted the ones in Main Menu to start again
    I totally deleted the scripts from the main menu in order to start again. For that, I did also a new ConnectionManager Script which is already populated and created new ones to accomplish my objective

---
+++ MultiplayerGame/Assets/Networking/MainMenu/ConnectionAPIScript.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class ConnectionAPIScript : MonoBehaviourPunCallbacks
+{
+    public int MaxPlayers = 10;
+    public string GameVersion = "1.0";
+
+    [SerializeField]
+    private GameObject RoomObject;
+    private RoomScript RoomManager;
+
+    [SerializeField]
+    private GameObject LobbyObject;
+    private LobbyScript LobbyManager;
+
+    [SerializeField]
+    private GameObject NetworkObject;
+    private NetworkUIScript NetUIManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectToNetwork();
+
+        NetUIManager = NetworkObject.GetComponent<NetworkUIScript>();
+        LobbyManager = LobbyManager.GetComponent<LobbyScript>();
+        RoomManager = RoomObject.GetComponent<RoomScript>();
+    }
+
+
+
+    // ----------------------------------------------------------------------
+    // ------------------------ CONNECTION FUNCTIONS ------------------------
+    public void ConnectToNetwork()
+    {
+        print("Connecting to Server");
+
+        // This allow us to use PhotonNetwork.LoadLevel() on the master and all clients in room will sync their level automatically
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+        // Set Settings
+        string name = "User" + Random.Range(0000, 9999);
+        PhotonNetwork.NickName = name;
+        PhotonNetwork.GameVersion = GameVersion;
+
+        // Connect and add callback
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.AddCallbackTarget(this.gameObject);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        print("Connecting to Master Server - Ready to Operate");
+
+        if (!PhotonNetwork.InLobby)
+            PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+
+    public override void OnJoinedLobby()
+    {
+        print("Lobby Joined Successfully");
+    }
+
+    public override void OnConnected()
+    {
+        print("Connected to Network");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("Disconnected from Server: " + cause.ToString());
+    }
+
+
+    // ----------------------------------------------------------------------
+    // ------------------------- SETTERS && GETTERS -------------------------
+    // --- Username ---
+    public void SetUsername(string name)    { PhotonNetwork.NickName = name; }
+    public string GetUsername()             { return PhotonNetwork.NickName; }
+    
+    // --- Status ---
+    public bool IsConnectedAndReady()       { return PhotonNetwork.IsConnectedAndReady; }
+
+    // --- Room Stuff ---
+    public string GetRoomName()
+    {
+        if (PhotonNetwork.InRoom)
+            return PhotonNetwork.CurrentRoom.Name;
+        else
+            return null;
+    }
+
+    public List<string> GetPlayerNamesInRoom()
+    {
+        List<string> ret = null;
+
+        if (PhotonNetwork.InRoom)
+        {
+            foreach (Player player in PhotonNetwork.PlayerList)
+                ret.Add(player.NickName);
+        }
+
+        return ret;
+    }
+
+
+
+
+
+    // ----------------------------------------------------------------------
+    // ------------------------ CONNECTION CALLBACKS ------------------------
+    
+}
