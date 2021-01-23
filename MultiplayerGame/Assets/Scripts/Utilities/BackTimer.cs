007d11081bd7f36405c0c10f450025f868fbe855
lucho1 <luchosuaya@hotmail.com>
2021-01-23 20:42:09 +0100

    Make bacwards timer

---
+++ MultiplayerGame/Assets/Scripts/Utilities/BackTimer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackTimer : MonoBehaviour
+{
+    // --- Simple Backwards Timer class ---
+    // It has a start time, a time in which it has to begin from
+    // To make it automatically start from game's beginning, check the Running bool to true
+    // To make it start from code, call Begin() whenever necessary
+    // To see if it's running just get this Running bool
+    public float StartTime = 10.0f;
+    public bool Running = false;
+
+    private float m_TimeRemaining;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        m_TimeRemaining = StartTime;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Running)
+        {
+            if (m_TimeRemaining > 0.0f)
+                m_TimeRemaining -= Time.deltaTime;
+            else
+            {
+                m_TimeRemaining = 0.0f;
+                Running = false;
+            }
+        }
+    }
+
+    public void Begin()
+    {
+        Running = true;
+        m_TimeRemaining = StartTime;
+    }
+
+    // --- Getters ---
+    public int GetMinutes()
+    {
+        return Mathf.FloorToInt((m_TimeRemaining + 1) / 60.0f);
+    }
+
+    public int GetSeconds()
+    {
+        return Mathf.FloorToInt((m_TimeRemaining + 1) % 60.0f);
+    }
+
+    public string GetTimeString()
+    {
+        return string.Format("{0:00}:{1:00}", GetMinutes(), GetSeconds());
+    }
+}
